a36ee82f8986c0c4658199596deef325996a8ab8
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-01 22:35:13 +0900

    21.04.01
    1.AlertView

---
+++ AlertView/AlertView/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  AlertView
+//
+//  Created by Derrick on 2021/04/01.
+//
+
+import SwiftUI
+
+// Alert View
+
+struct ContentView: View {
+    //-----------
+    // Properties
+    //-----------
+    
+    @State private var isShowAlert = false
+    @State private var selectText = "X"
+    
+    var body: some View {
+        
+        VStack{
+            Spacer()
+            // True 일경우만 Alert창이 나오도록
+            Button("Show Alert") {
+                // toggle은 반전 되는것
+                isShowAlert.toggle()
+            }
+            // .alert는 기본적으로 ture이면 나옴.
+            .alert(isPresented: $isShowAlert, content: {
+//                Alert(title: Text("Alert!!") )
+//                // ,dismissButton: .cancel() 쓰면 OK가 cancel로 바뀜
+                let primaryButton = Alert.Button.default(Text("Done")) {
+                    selectText = "Done"
+                }
+                let secondaryButton = Alert.Button.default(Text("Cancel")) {
+                    selectText = "Cancel"
+                }
+                
+                return Alert(title: Text("Show Alert!"), primaryButton: primaryButton, secondaryButton: secondaryButton )
+                
+                
+            })
+            // Spacer는 맨 밑으로
+             Spacer()
+             Text("\(isShowAlert.description)")
+             Spacer()
+             Text("\(selectText)")
+             Spacer()
+        }
+        
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- ViewModifier/ViewModifier/ContentView.swift
+++ ViewModifier/ViewModifier/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Main Title")
+                .font(.largeTitle)
+                .foregroundColor(.black )
+                .bold()
+             
+            Text("Sub Title")
+                .modifier(MyTextStyle(myWeight: .bold, myFont: .title))
+            
+            Text("Description")
+                .modifier(MyTextStyle(myWeight: .bold, myFont: .title2, myColor: .red))
+            
+            Text("Description2")
+                .customFont(color: .gray)
+        }
+        
+    }
+}
+
+// 똑같은 설정을 최대한 줄이기 위해
+// ViewModifier 에서는 italic 체를 사용할수없다.
+struct MyTextStyle: ViewModifier {
+    
+    var myWeight = Font.Weight.regular
+    
+    var myFont = Font.title3.weight(.bold)
+    
+    var myColor = Color.black
+    
+    func body(content: Content) -> some View {
+        
+        content
+            .font(myFont.weight(myWeight))
+            .foregroundColor( myColor)
+            .padding(.bottom, 20)
+        
+    }
+}
+// italic, bold 넣고싶으면 extension을 쓴다
+// 따로 struct로 설정해서 안되는 경우 extension을 활용한다.
+extension Text{
+    func customFont(color : Color) -> Text {
+        self
+            .font(.title2)
+            .bold()
+            .italic()
+            .foregroundColor(color)
     }
 }
 
