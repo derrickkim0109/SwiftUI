3457662e6c4e9ca8e6da5037f4b62016d9280faa
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-13 18:01:03 +0900

    21.04.13_ScrollViewReader_ Incomplete

---
+++ ScrollViewReader/ScrollViewReader/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  ScrollViewReader
+//
+//  Created by Derrick kim on 2021/04/13.
+//
+
+import SwiftUI
+
+//ScrollViewReader
+
+struct ContentView: View {
+    
+    //-----------
+    // Properties
+    //-----------
+    @State private var proxy: ScrollViewProxy?
+    
+    
+    var body: some View {
+        
+        VStack{
+            
+            Button("Scroll To"){
+                proxy?.scrollTo(15)
+            }
+            
+            ScrollView{
+                
+                ScrollViewReader(content: { proxy in
+                    // self.proxy = proxy <- 여기서는 불가능 : 화면을 계속그리는 부분이기 때문에.
+                    
+                    ForEach(0..<50){ index in
+                        Text("\(index)")
+                            .padding()
+                    }
+                    // Data setting 된후
+                    .onAppear(perform: {
+                        self.proxy = proxy
+                    })
+                    
+                })
+               
+            }
+        }
+        
+       
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
