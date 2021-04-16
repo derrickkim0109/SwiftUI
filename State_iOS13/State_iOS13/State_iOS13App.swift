91ec0d8503838e80ccc58363f06dcf3a3b7e159b
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-04-16 16:28:12 +0900

    21.04.16_State_iOS13

---
+++ State_iOS13/State_iOS13/State_iOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  State_iOS13App.swift
+//  State_iOS13
+//
+//  Created by Derrick kim on 2021/04/16.
+//
+
+import SwiftUI
+
+@main
+struct State_iOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
